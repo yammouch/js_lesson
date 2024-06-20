51de962f4bbbe5f8fe16c62273752c519bb0587b
Takuya Yamauchi <tyama@preferred.jp>
2024-06-21 08:20:30 +0900

    The sound source is separated into a class.

--- 0020_audio/sq-pr.js
+++ 0020_audio/sq-pr.js
@@ -1,25 +1,38 @@
-// white-noise-processor.js
+class Source {
+
+  constructor() {
+    this.phase = 0;
+  }
+
+  pop() {
+    let retval;
+    if (this.phase < 50) {
+      retval = -1.0;
+    } else {
+      retval = 1.0;
+    }
+    if (99 <= this.phase) {
+      this.phase = 0;
+    } else {
+      this.phase++;
+    }
+    return retval;
+  }
+
+}
+
 class SquareProcessor extends AudioWorkletProcessor {
 
   constructor () {
     super();
-    this.phase = 0;
+    this.src = new Source();
   }
 
   process(inputs, outputs, parameters) {
     const output = outputs[0];
     const channel = output[0];
     for (let i = 0; i < channel.length; i++) {
-      if (this.phase < 50) {
-        channel[i] = -1.0;
-      } else {
-        channel[i] = 1.0;
-      }
-      if (99 <= this.phase) {
-        this.phase = 0;
-      } else {
-        this.phase++;
-      }
+      channel[i] = this.src.pop();
     }
     return true;
   }
