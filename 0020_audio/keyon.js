20b0c829e4f8fde28577e91b8670f27a470d0d46
Takuya Yamauchi <tyama@preferred.jp>
2024-06-24 14:27:41 +0900

    Decay has been added.

--- 0020_audio/keyon.js
+++ 0020_audio/keyon.js
@@ -3,21 +3,29 @@ class Source {
   constructor() {
     this.phase = 0;
     this.on = false;
+    this.wave = new Float32Array(100);
+    this.buf = new Float32Array(this.wave.length);
+    for (let i = 0; i < 50; i++) {
+      this.wave[i] = 1.0;
+    }
+    for (let i = 50; i < 100; i++) {
+      this.wave[i] = -1.0;
+    }
+    this.excite = this.wave.length;
+    this.decay = 1 - 1e-1;
   }
 
   pop() {
     let retval;
-    if (!this.on) {
-      retval = 0.0;
-    } else if (this.phase < 50) {
-      retval = -1.0;
+    retval = this.buf[this.phase];
+    if (this.excite < this.buf.length) {
+      this.buf[this.phase] = this.wave[this.excite++];
     } else {
-      retval = 1.0;
+      this.buf[this.phase] *= this.decay;
     }
-    if (99 <= this.phase) {
+    this.phase++;
+    if (this.buf.length <= this.phase) {
       this.phase = 0;
-    } else {
-      this.phase++;
     }
     return retval;
   }
@@ -31,8 +39,16 @@ class SquareProcessor extends AudioWorkletProcessor {
     this.src = new Source();
     this.port.onmessage = (e) => {
       if (e.data == "on") {
+        if (!this.src.on) {
+          console.log("on");
+          this.src.excite = 0;
+          this.src.decay = 1 - 5e-3;
+          this.src.on = true;
+        }
         this.src.on = true;
       } else if (e.data == "off") {
+        console.log("off");
+        this.src.decay = 1 - 1e-1;
         this.src.on = false;
       }
     };
