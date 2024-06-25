2891b14beb32b183b6dcf7ff8464dd3773890706
Takuya Yamauchi <tyama@preferred.jp>
2024-06-25 13:18:04 +0900

    Updated.

--- 0020_audio/keyon.js
+++ 0020_audio/keyon.js
@@ -43,22 +43,32 @@ class SquareProcessor extends AudioWorkletProcessor {
 
   constructor() {
     super();
-    this.src = new Source(100.1);
-    //this.src = new Source(99.9);
-    //this.src = new Source(100);
+    let src = [];
     this.port.onmessage = (e) => {
-      if (e.data == "on") {
-        if (!this.src.on) {
-          console.log("on");
-          this.src.excite = 0;
-          this.src.decay = 1 - 5e-3;
-          this.src.on = true;
+      console.log(e.data);
+      if (e.data.cmd == "on") {
+        console.log(e.data.note);
+        console.log(src[e.data.note]);
+        console.log(src);
+        this.src[e.data.note].excite = 0;
+        this.src[e.data.note].decay = 1 - 5e-3;
+      } else if (e.data.cmd == "off") {
+        this.src[e.data.note].decay = 1 - 1e-1;
+      } else if (e.data.cmd == "init") {
+        let note = -34;
+        //this.src = [];
+        for (let i = 0; i <= 37; i++, note++) {
+          //this.src.push
+          src.push
+          ( new Source
+            ( e.data.sampleRate
+            / (e.data.master * 2**(note/12))
+            ));
         }
-        this.src.on = true;
-      } else if (e.data == "off") {
-        console.log("off");
-        this.src.decay = 1 - 1e-1;
-        this.src.on = false;
+        console.log(src);
+        console.log(src[0]);
+        console.log(src[37]);
+        this.src = src;
       }
     };
   }
@@ -67,7 +77,11 @@ class SquareProcessor extends AudioWorkletProcessor {
     const output = outputs[0];
     const channel = output[0];
     for (let i = 0; i < channel.length; i++) {
-      channel[i] = this.src.pop();
+      let sum = 0.0;
+      for (let j = 0; j < this.src.length; j++) {
+        sum += this.src[j].pop();
+      }
+      channel[i] = sum;
     }
     return true;
   }
