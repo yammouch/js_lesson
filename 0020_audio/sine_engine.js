9105a3d047993ac375c4be3e2d54a49a82b9ae54
Takuya Yamauchi <yammouch@gmail.com>
2024-07-22 12:59:10 +0900

    Updated.

--- 0020_audio/sine_engine.js
+++ 0020_audio/sine_engine.js
@@ -3,7 +3,7 @@ export class Sine {
   constructor(init_event) {
     this.init_event = init_event;
     this.phase = 0.0;
-    this.coeffs = new Float32Array(10);
+    this.coeffs = new Float32Array(150);
     for (let i = 0; i < this.coeffs.length; i++) {
       this.coeffs[i] = 1.0/((2*i+2)*(2*i+3));
     }
@@ -19,12 +19,22 @@ export class Sine {
     return acc;
   }
 
+  harms(n, x) {
+    let acc = 0.0;
+    const pi2 = 2*Math.PI;
+    for (let i = n; 0 < i; i--) {
+      let phase = i*x-pi2*Math.floor(i*x/pi2);
+      acc += this.sine(phase)/i;
+    }
+    return acc;
+  }
+
   pop() {
     this.phase += 2 * Math.PI * 440 / 48000;
     if (2 * Math.PI < this.phase) {
       this.phase -= 2 * Math.PI;
     }
-    return this.sine(this.phase);
+    return this.harms(20, this.phase);
   }
 
 }
